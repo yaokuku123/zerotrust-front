4ac07e62a3bc10e65daf3520a1de045305959444
yaokuku123 <yaoqijun@outlook.com>
2021-07-07 17:10:49 +0800

    info content

--- src/api/soft/soft-info.js
+++ src/api/soft/soft-info.js
@@ -1,10 +1,26 @@
 import request from '@/utils/request'
 
 export default {
+  //获取当前系统中存在的软件交易信息
   getSoftList() {
       return request({
           url: '/soft/list',
           method: 'get'
+        })
+  },
+  //添加被测软件的相关信息资料
+  addSoft(softInfo) {
+      return request({
+          url: '/soft/add',
+          method: 'post',
+          data: softInfo
+        })
+  },
+  //审核当前被测软件
+  softVerify(id) {
+      return request({
+          url: '/soft/verity/'+id,
+          method: 'post'
         })
   }
 }
--- src/views/soft/info.vue
+++ src/views/soft/info.vue
@@ -8,18 +8,54 @@
       <el-step title="提交审核"/>
     </el-steps>
 
-    <el-form label-width="120px">
-      <el-form-item>
-        <el-button :disabled="saveBtnDisabled" type="primary" @click="next">保存并下一步</el-button>
+    <el-form :model="softInfo" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+      <el-form-item label="软件名称" prop="softName">
+        <el-input v-model="softInfo.softName"></el-input>
+      </el-form-item>
+      <el-form-item label="软件描述" prop="softDesc">
+        <el-input v-model="softInfo.softDesc"></el-input>
+      </el-form-item>
+      <el-form-item label="软件拥有者" prop="userName">
+        <el-input v-model="softInfo.userName"></el-input>
+      </el-form-item>
+      <el-form-item label="手机号" prop="phoneNum">
+        <el-input v-model="softInfo.phoneNum"></el-input>
+      </el-form-item>
+
+       <el-form-item>
+        <el-button :disabled="saveBtnDisabled" type="primary" @click="next()">保存并下一步</el-button>
       </el-form-item>
     </el-form>
   </div>
 </template>
 <script>
+import soft from '@/api/soft/soft-info'
+
 export default {
   data() {
     return {
-      saveBtnDisabled: false// 保存按钮是否禁用
+      saveBtnDisabled: false,// 保存按钮是否禁用
+      softInfo: {
+          id: 0,
+          softName: '',
+          softDesc: '',
+          userName: '',
+          phoneNum: ''
+      },
+      rules: {
+        softName: [
+          { required: true, message: '请输入软件名称', trigger: 'blur' }
+        ],
+        softDesc: [
+          { required: true, message: '请输入软件描述', trigger: 'blur' }
+        ],
+        userName: [
+          { required: true, message: '请输入软件拥有者', trigger: 'blur' }
+        ],
+        phoneNum: [
+          { required: true, message: '请输入手机号', trigger: 'blur' }
+        ]
+      }
     }
   },
   created() {
@@ -28,7 +64,11 @@ export default {
   methods: {
     next() {
       console.log('next')
-      this.$router.push({ path: '/soft/upload/1' })
+      // soft.addSoft(this.softInfo).then(response =>{//请求成功
+      //               //response接口返回的数据
+      //               this.softInfo.id = response.data.id
+      // })
+      this.$router.push({ path: '/soft/upload/'+this.softInfo.id })
     }
   }
 }
