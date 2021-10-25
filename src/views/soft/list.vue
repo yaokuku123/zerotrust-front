80996343b541894594dcf511affa3476e9d73a50
yaokuku123 <yaoqijun@outlook.com>
2021-10-25 22:39:36 +0800

    调整数据显示顺序，添加编号和excel下载按钮

--- src/views/soft/list.vue
+++ src/views/soft/list.vue
@@ -12,12 +12,18 @@
       <el-step title="归档" />
       <el-step title="浏览" />
     </el-steps>
+    <el-button
+            type="primary"
+            size="medium"
+            @click="exportExcel()"
+    >导出Excel</el-button>
     <!-- 表格 -->
     <el-table :data="softList" stripe style="width: 100%">
-      <el-table-column prop="pid" label="编号" width="480" />
-      <el-table-column prop="comName" label="单位名称" width="380" />
-      <el-table-column prop="proName" label="项目名称" width="380" />
+      <el-table-column label="编号" type="index" width="80" /> 
+      <el-table-column prop="proName" label="项目名称" width="300" />
+      <el-table-column prop="comName" label="单位名称" width="300" />
       <el-table-column prop="verificationCode" label="核验码" width="380" />
+      <el-table-column prop="pid" label="证书编号" width="480" />
       <el-table-column fixed="right" label="操作" >
         <template slot-scope="scope">
           <el-button
@@ -37,6 +43,7 @@ import softVerify from '@/api/soft/soft-verify'
 export default {
   data() {
     return {
+      baseCertDownloadUrl: process.env.VUE_APP_BASE_API,
       softList: [
         {
           sid:'',
@@ -100,6 +107,11 @@ export default {
       console.log(id)
       this.$router.push({ path: '/soft/back/' + id, query: { id: id }})
     },
+    exportExcel(){
+      window.open(
+        this.baseCertDownloadUrl + "/excel" 
+      );
+    }
     // getSoft() {
     //   soft.getSoftList().then((response) => {
     //     // 请求成功
